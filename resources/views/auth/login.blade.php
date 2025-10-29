97bbb19ef0037efe3afdc285b10fb83de2e8ad6e
igorgudrian <igorgudrian@gmail.com>
2025-10-29 12:14:24 -0300

    Add files via upload
    resources

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,38 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+<?php
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+namespace App\Http\Controllers\Auth;
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Usuario;
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'nome' => 'required|string',
+            'senha' => 'required|string'
+        ]);
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+        $user = Usuario::where('nome', $credentials['nome'])->first();
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+        if ($user && $user->senha === $credentials['senha']) {
+            Auth::login($user);
+            return redirect()->intended('/dashboard');
+        }
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
+        return back()->withErrors([
+            'nome' => 'Nome de usuário ou senha incorretos.',
+        ])->withInput();
+    }
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/');
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,32 +7,64 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
- 
+  
     <main>
+    <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="loginModalLabel">Login</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <form method="POST" action="{{ route('login') }}">
+        @csrf
+        <div class="modal-body">
+          @if($errors->any())
+            <div class="alert alert-danger">
+              {{ $errors->first() }}
+            </div>
+          @endif
+          
+          <div class="form-group">
+            <input placeholder="Nome" type="text" class="form-control @error('nome') is-invalid @enderror" id="nome" name="nome" value="{{ old('nome') }}" required>
+          </div>
+          <div class="form-group">
+            <input placeholder="Senha" type="password" class="form-control @error('senha') is-invalid @enderror" id="senha" name="senha" required>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+          <button type="submit" class="btn btn-primary">Entrar</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
       <h1 class="text-center navbar-dark bg-warning">Sabor do Brasil</h1>
       <div class="container">
-        <div class="row">
-          <div class="col">
-              <img class="rounded mx-auto d-block" src="{{ asset($usuario->foto) }}" alt="Foto do usuário" style="width: 150px; height: 150px; object-fit: cover;">
-            <h5 class="text-center border-bottom border-dark">{{ $usuario->nome }}</h5>
-          <div class="container">
-           <div class="row">
-            <div class="col">
-               <h3 class="text-center">{{ $totalLikes ?? 0 }}</h3>
-               <h6 class="text-center">Likes Totais</h6>
-             </div>
-              <div class="col">
-                <h3 class="text-center">{{ $totalDislikes ?? 0 }}</h3>
-                <h6 class="text-center">Dislikes Totais</h6>
-             </div>
-            </div>
-           </div>
-          </div>
-
-          <div class="col-6 border-left border-right border-dark">
-            <h5 class="text-center mb-4">Publicações</h5>
-            <div class="d-flex flex-column align-items-center">
-          @foreach($publicacoes as $publicacao)
+  <div class="row">
+   <div class="col">
+    <img class="rounded mx-auto d-block" src="{{asset('imagens/logo.png')}}" alt="logo">
+    <h5 class="text-center border-bottom border-dark">Sabor do Brasil</h5>
+    <div class="container">
+  <div class="row">
+    <div class="col">
+      <h3 class="text-center">{{ $totalLikes ?? 0 }}</h3>
+      <h6 class="text-center">Likes Totais</h6>
+    </div>
+     <div class="col">
+      <h3 class="text-center">{{ $totalDislikes ?? 0 }}</h3>
+      <h6 class="text-center">Dislikes Totais</h6>
+     </div>
+    </div>
+   </div>
+  </div>
+  <div class="col-6 border-left border-right border-dark">
+<h5 class="text-center mb-4">Publicações</h5>
+<div class="d-flex flex-column align-items-center">
+  @foreach($publicacoes as $publicacao)
 <div class="border border-dark p-3 mb-4 w-75 text-center rounded">
     <h6>{{$publicacao->titulo_prato}}</h6>
     <img src="{{asset($publicacao->foto)}}" alt="{{$publicacao->titulo_prato}}" class="img-fluid mb-2" style="max-height: 200px; object-fit: cover;">
@@ -142,41 +174,37 @@
     @endif
 </div>
 @endforeach
-        </div>
-          </div>
-          <div class="col text-center">
-            <form method="POST" action="{{ route('logout') }}">
-              @csrf
-              <button type="submit" class="btn btn-danger btn-lg text-center">
-                Sair
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
+</div>
+  </div>
+    <div class="col text-center">
+     <button type="button" class="btn btn-danger btn-lg text-center" data-toggle="modal" data-target="#loginModal">
+        Entrar
+     </button>
+    </div>
+</div>
+</div>
     </main>
 
     <footer class="bg-danger text-white container-fluid sticky-footer">
       <div class="container">
-        <div class="row">
-          <div class="col">
-            <p class="text-center">Sabor do Brasil</p>
-          </div>
-          <div class="col-6">
-            <nav class="nav row">
-              <a class="nav-link text-center col" href=""><img src="{{ asset('imagens/Instagram.svg') }}" alt="Insta"></a>
-              <a class="nav-link text-center col" href=""><img src="{{ asset('imagens/Whatsapp.svg') }}" alt="Whatss"></a>
-              <a class="nav-link text-center col" href=""><img src="{{ asset('imagens/Twitter.svg') }}" alt="Twitter"></a>
-              <a class="nav-link text-center col" href=""><img src="{{ asset('imagens/Globe.svg') }}" alt="Goggle"></a>
-            </nav>
-          </div>
-          <div class="col">
-            <p>&copy; Direitos Autorais 2025</p>
-          </div>
+      <div class="row">
+        <div class="col">
+          <p class="text-center">Sabor do Brasil</p>
+        </div>
+        <div class="col-6 ">
+          <nav class="nav row">
+            <a class="nav-link text-center col" href=""><img src="{{ asset('imagens/Instagram.svg') }}" alt="Insta"></a>
+            <a class="nav-link text-center col" href=""><img src="{{ asset('imagens/Whatsapp.svg') }}" alt="Whatss"></a>
+            <a class="nav-link text-center col" href=""><img src="{{ asset('imagens/Twitter.svg') }}" alt="Twitter"></a>
+            <a class="nav-link text-center col" href=""><img src="{{ asset('imagens/Globe.svg') }}" alt="Goggle"></a>
+          </nav>
+        </div>
+        <div class="col">
+          <p>&copy; Direitos Autorais 2025</p>
         </div>
       </div>
+      </div>
     </footer>
-
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
